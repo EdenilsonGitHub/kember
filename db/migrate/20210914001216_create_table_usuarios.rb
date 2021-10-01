a2d393d02d5e12a0a55d8bcf7786c9807635c7ee
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-09-30 22:15:21 -0300

    KMB-18 - Criando tabela para o projeto

--- db/migrate/20210914001216_create_table_usuarios.rb
+++ db/migrate/20210914001216_create_table_usuarios.rb
@@ -1,10 +1,10 @@
 class CreateTableUsuarios < ActiveRecord::Migration[6.1]
   def change
     create_table :usuarios do |t|
-      t.string :nome
-      t.string :login
-      t.string :senha
-      t.string :email
+      t.string  :nome
+      t.string  :login
+      t.string  :senha
+      t.string  :email
       t.boolean :admin
       t.timestamps
     end
