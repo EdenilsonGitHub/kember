e805f318b4a03cf01a67c7f5feabdf2b2f7b53ad
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-10-04 23:06:19 -0300

    KMB-11 - Criando modelo da empresa

---
+++ app/models/empresa.rb
@@ -0,0 +1,15 @@
+class Empresa < ActiveRecord::Base
+    self.table_name = 'empresas'
+
+    include Paperclip::Glue
+
+    validates :nome, :nome_fantasia, :sigla, :cnpj, :email, presence: true
+    validates :email, :cnpj, :nome, uniqueness: true    
+    validates :cnpj, length: { minimum: 14, message: 'é muito curto (mínimo: 14 caracteres)' }
+    validates :cnpj, length: { maximum: 18, message: 'é muito longo (máximo: 18 caracteres)' }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    
+    has_attached_file :logo, styles: { normal: "300x300>" }
+    validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
+
+end 
