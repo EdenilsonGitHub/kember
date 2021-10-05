4e6ad445be4da6f89a6db6f6e75db76bae56cc8b
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-10-04 23:25:34 -0300

    KMB-27 - Criando modelo da sprint

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,13 +1,11 @@
-class Projeto < ActiveRecord::Base
-    self.table_name = 'projetos'
+class Sprint < ActiveRecord::Base
+    self.table_name = 'sprints'
 
-    validates :nome, :descricao, :inicio_projeto, :fim_projeto, :empresa_id, presence: true
+    validates :nome, :descricao, :inicio_sprint, :fim_sprint, :projeto_id, presence: true
     validates :nome, length: { minimum: 5, message: 'é muito curto (mínimo: 5 caracteres)' }
     validates :descricao, length: { minimum: 10, message: 'é muito curto (mínimo: 10 caracteres)' }
     validates :nome, length: { maximum: 255, message: 'é muito longo (máximo: 255 caracteres)' }
     
-    belongs_to :empresa
-
-    has_many :sprints
+    belongs_to :projeto
 
 end 
