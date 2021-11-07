7c159e55b5ce0fc61edcfeef1d75e47f37506d09
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-11-07 19:43:37 -0300

    KMB-66 - Criação da função next conforme as prioridades

--- app/controllers/quadros_controller.rb
+++ app/controllers/quadros_controller.rb
@@ -5,6 +5,12 @@ class QuadrosController < ApplicationController
         @projeto = Projeto.find_by_id(params[:projeto_id])
         @colunas = @projeto.colunas
         @sprint = Sprint.find_by_id(params[:sprint_id])
+        @colunas_ids = @projeto.colunas.pluck(:id)
+        @hash_quadros = {}
+        @colunas_ids.each do |col|
+            @hash_quadros[col] = {}
+            @hash_quadros[col] = @colunas.find_by_id(col).quadros.where(sprint_id: @sprint.id)
+        end
         if @quadro.save
         else
             render action: 'new'
