7f57a764aee48084923a6b92b5296fe6e8d05002
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-11-08 00:06:11 -0300

    KMB-77 - Adicionado migração para o status de lista

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_225104) do
+ActiveRecord::Schema.define(version: 2021_11_08_025318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_225104) do
     t.integer "projeto_id"
     t.integer "rank", default: 1
     t.boolean "finalizador", default: false
+    t.boolean "lista", default: false
   end
 
   create_table "usuarios", force: :cascade do |t|
