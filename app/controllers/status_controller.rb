4f9856a95399f0202c3de68f809b855a3a245cc2
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-11-15 20:00:14 -0300

    KMB-55 - Criação do status

--- app/controllers/status_controller.rb
+++
@@ -1,8 +0,0 @@
-class StatusController < ApplicationController
-
-    def show
-        @projeto = Projeto.find_by_id(params[:projeto_id])
-        @status = @projeto.status.order('rank ASC')
-    end
-
-end
