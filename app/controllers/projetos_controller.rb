6fe781586e3517f034dc2bc7d2b0375166845e41
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-11-15 18:01:55 -0300

    KMB-24 - Criando new/create do projeto

--- app/controllers/projetos_controller.rb
+++ app/controllers/projetos_controller.rb
@@ -1,7 +1,22 @@
 class ProjetosController < ApplicationController
 
     def index
-        @projetos = Projeto.all
+        @projetos = Projeto.where(id: UsuarioProjeto.where(usuario_id: @usuario_logado.id).pluck(:projeto_id))
+    end
+
+    def new
+        @projeto = Projeto.new
+    end
+
+    def create
+        @projeto = Projeto.new(params.require(:projeto).permit(:nome, :sigla, :descricao, :email, :inicio_projeto, :fim_projeto, :usuario_id, :empresa_id))
+        if @projeto.save
+            @usuario_projeto = UsuarioProjeto.new(usuario_id: params[:usuario_id], projeto_id: @projeto.id)
+            @usuario_projeto.save
+            redirect_to projetos_path
+        else
+            render action: 'new'
+        end
     end
 
     def show
