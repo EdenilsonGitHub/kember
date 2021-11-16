cfe7ec6683853fced2abe20d0ef8f1987bcd07a7
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-11-15 22:20:48 -0300

    KMB-16 - New/create da empresa

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -1,3 +1,16 @@
 class EmpresasController < ApplicationController
 
+    def new
+        @empresa = Empresa.new
+    end
+
+    def create
+        @empresa = Empresa.new(params.require(:empresa).permit(:nome, :nome_fantasia, :sigla, :cnpj, :telefone, :email, :logo, :ativo))
+        if @empresa.save
+            redirect_to empresas_path
+        else
+            render action: 'new'
+        end
+    end
+
 end
