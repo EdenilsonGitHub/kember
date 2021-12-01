1ef6d3f8396c03fd6401b310420299301ba87503
EdenilsonGitHub <edenilson@etica-ti.com.br>
2021-12-01 20:12:26 -0300

    KMB-85 - Ajuste de abas selecionadas e em breve de páginas não finalizadas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,15 @@
 class HomeController < ApplicationController
 
     def index
+        @aba_selecionada = "inicio"
     end
 
     def gerenciamento
+        @aba_selecionada = "projetos"
+    end
+
+    def em_breve
+        @aba_selecionada = params[:selecionada] 
     end
 
 end
